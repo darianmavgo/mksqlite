0800375637fb1e9923d8ded5e8cc8473136b6b4a
Darian Hickman <darian.hickman@mavgo.com>
2026-01-16 13:33:59 -0500

    .gitignore

---
+++ test_output/csv_convert.sql
@@ -0,0 +1,5 @@
+CREATE TABLE data (clname TEXT, clage TEXT, clcity TEXT);
+
+INSERT INTO data (clname, clage, clcity) VALUES ('John', '25', 'New York');
+INSERT INTO data (clname, clage, clcity) VALUES ('Jane', '30', 'London');
+INSERT INTO data (clname, clage, clcity) VALUES ('Bob', '35', 'Paris');
