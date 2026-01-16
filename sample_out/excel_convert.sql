fc1d0d7c335aded53e919e0ba6e332e7bea590df
Darian Hickman <darian.hickman@mavgo.com>
2026-01-16 13:23:36 -0500

    Removed files from git tracking that should not be there.

--- sample_out/excel_convert.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sheet1 (name TEXT, age TEXT, city TEXT);
-
-INSERT INTO sheet1 (name, age, city) VALUES ('Alice', '28', 'Boston');
-INSERT INTO sheet1 (name, age, city) VALUES ('Bob', '32', 'Seattle');
-INSERT INTO sheet1 (name, age, city) VALUES ('Charlie', '45', 'Austin');
-
