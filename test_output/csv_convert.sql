aa4fd67c5abe2c691c03bf6b0e7943b4f9a40733
Darian Hickman <darian.hickman@mavgo.com>
2026-01-16 13:39:23 -0500

    Clearing more bad tracking

--- test_output/csv_convert.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE data (clname TEXT, clage TEXT, clcity TEXT);
-
-INSERT INTO data (clname, clage, clcity) VALUES ('John', '25', 'New York');
-INSERT INTO data (clname, clage, clcity) VALUES ('Jane', '30', 'London');
-INSERT INTO data (clname, clage, clcity) VALUES ('Bob', '35', 'Paris');
